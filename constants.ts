5b193e288b3ce119547d933a68971c2a66ae7173
Ajith-leet <ajithkumar@paperflite.com>
2025-01-07 21:32:11 +0530

    Initial commit

---
+++ constants.ts
@@ -0,0 +1,15 @@
+export const images = [
+    "https://images.pexels.com/photos/19727180/pexels-photo-19727180/free-photo-of-a-house-in-the-snow-with-trees-and-snow.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/19570330/pexels-photo-19570330/free-photo-of-cold-snow-wood-dirty.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/27643389/pexels-photo-27643389/free-photo-of-santo-amaro-lencois-maranhenses.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/16233277/pexels-photo-16233277/free-photo-of-dog-standing-on-lakeshore.png?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29851698/pexels-photo-29851698/free-photo-of-artisan-desserts-with-strawberry-ice-cream.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29843712/pexels-photo-29843712/free-photo-of-elegant-european-building-with-red-roof.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29843712/pexels-photo-29843712/free-photo-of-elegant-european-building-with-red-roof.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29933654/pexels-photo-29933654/free-photo-of-majestic-glacier-in-obergoms-switzerland.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29203755/pexels-photo-29203755/free-photo-of-solo-fisherman-on-moroccan-boat-under-red-umbrella.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://images.pexels.com/photos/29145537/pexels-photo-29145537/free-photo-of-ancient-stone-wall-with-wooden-doorway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/29865382/pexels-photo-29865382/free-photo-of-colorful-beach-cabin-doors-in-le-havre-france.jpeg?auto=compress&cs=tinysrgb&w=1200&h=630",
+    "https://plus.unsplash.com/premium_photo-1734626880757-a107821e718e?w=1200&h=630&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1719937050446-a121748d4ba0?w=1200&h=630&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
+];
